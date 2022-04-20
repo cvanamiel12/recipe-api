c73560b7c83bf01a55af597d9601b905ae1cf521
cvanamiel12 <cvanamiel@gmail.com>
2022-04-20 18:31:23 +0800

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Popular from "../components/Popular";
+
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <Popular/>
+
+    </div>
+  )
+}
+
+export default Home;
